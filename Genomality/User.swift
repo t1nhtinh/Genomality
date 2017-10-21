261665790d602c055b370052cc4047cf962c2085
Tinh Dang <tinhminhdang@gmail.com>
2017-10-21 16:24:53 -0700

    User object added

---
+++ Genomality/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  Genomality
+//
+//  Created by Tinh Dang on 10/21/17.
+//  Copyright © 2017 Tinh Dang. All rights reserved.
+//
+
+import UIKit
+
+class User {
+    var name: String
+    var phenoTypeScore: Int
+    
+    
+    
+    init(name: String, phenoTypeScore: Int) {
+        self.name = name
+        self.phenoTypeScore = phenoTypeScore
+        
+    }
+    
+}
